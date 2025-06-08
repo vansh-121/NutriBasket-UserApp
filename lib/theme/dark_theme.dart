8c93822543ee10e0d8e1205544e066e3577a380b
Vansh <vansh.sethi98760@gmail.com>
2025-06-08 14:25:49 +0530

    Light Mode & Dark Mode Colors Changed

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sixam_mart/util/app_constants.dart';
 
-ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
+ThemeData dark({Color color = const Color.fromARGB(255, 239, 217, 24)}) =>
+    ThemeData(
       fontFamily: AppConstants.fontFamily,
       primaryColor: color,
-      secondaryHeaderColor: const Color(0xFF009f67),
+      secondaryHeaderColor: const Color.fromARGB(255, 239, 217, 24),
       disabledColor: const Color(0xffa2a7ad),
       brightness: Brightness.dark,
       hintColor: const Color(0xFFbebebe),
--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sixam_mart/util/app_constants.dart';
 
-ThemeData light({Color color = const Color(0xFF039D55)}) => ThemeData(
+ThemeData light({Color color = const Color.fromARGB(255, 164, 15, 4)}) =>
+    ThemeData(
       fontFamily: AppConstants.fontFamily,
       primaryColor: color,
-      secondaryHeaderColor: const Color(0xFF1ED7AA),
+      secondaryHeaderColor: Color.fromARGB(255, 164, 15, 4),
       disabledColor: const Color(0xFFBABFC4),
       brightness: Brightness.light,
       hintColor: const Color(0xFF9F9F9F),
@@ -14,7 +15,7 @@ ThemeData light({Color color = const Color(0xFF039D55)}) => ThemeData(
           style: TextButton.styleFrom(foregroundColor: color)),
       colorScheme: ColorScheme.light(primary: color, secondary: color)
           .copyWith(surface: const Color(0xFFFCFCFC))
-          .copyWith(error: const Color(0xFFE84D4F)),
+          .copyWith(error: Color.fromARGB(255, 164, 15, 4)),
       popupMenuTheme: const PopupMenuThemeData(
           color: Colors.white, surfaceTintColor: Colors.white),
       dialogTheme: const DialogTheme(surfaceTintColor: Colors.white),
